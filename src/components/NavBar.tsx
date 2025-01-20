0051d8d217fd6f5e959106ec4ee27914b79084fb
angella naigaga <anjayluh.wakabi@gmail.com>
2025-01-20 12:23:24 +0300

    Added logo to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
@@ -12,15 +13,26 @@ export default function NavBar({ menuItems }: NavBarProps) {
 
     return (
         <nav className="w-64 bg-gray-800 text-white">
-            <div className="p-6">
-                <ul>
+            <div className="p-6 flex flex-col items-center">
+                <div className="mb-6">
+                    <Image
+                        src="/images/logo.png"
+                        alt="Logo"
+                        width={50}
+                        height={50}
+                        className="object-contain"
+                    />
+                </div>
+
+                {/* Menu Items */}
+                <ul className="w-full">
                     {menuItems.map((item, index) => (
                         <li key={index} className="mb-2">
                             <Link
                                 href={item.href}
                                 className={`block px-4 py-2 rounded ${
                                     pathname === item.href
-                                        ? 'bg-gray-700' // Apply the same hover color when active
+                                        ? 'bg-gray-700'
                                         : 'hover:bg-gray-700'
                                 }`}
                             >
