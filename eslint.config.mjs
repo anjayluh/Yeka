2ca22cab1dfe957abbe0d81ebbd4a250b600a39a
angella naigaga <anjayluh.wakabi@gmail.com>
2025-02-01 21:06:51 +0300

    deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+       "@typescript-eslint/no-unused-vars": "off",
+       "@next/next/no-page-custom-font": "off",
+       "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "off"
+  }
+  }
 ];
 
 export default eslintConfig;
